2f83df1a9042ef16c76cebd4faad71cfead5dac1
John Doe <john.doe@example.com>
2024-11-22 21:37:51 +0100

    modified:   src/components/Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useState } from 'react';
 import { motion } from 'framer-motion';
-import Image from 'next/image';
 import Marquee from './ui/Marquee';
 
 const Home = () => {
